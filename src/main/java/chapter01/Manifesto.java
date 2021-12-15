792543376b0e4f989ca9446cfc7b6186cc2b230d
neroHua <2676337735@qq.com>
2021-12-15 22:16:47 +0800

    add manifesto

---
+++ src/main/java/chapter01/Manifesto.java
@@ -0,0 +1,23 @@
+package chapter01;
+
+/**
+ * manifesto For Agile Software Development
+ * 敏捷开发宗旨
+ *
+ * individuals and interactions over processes and tool
+ * 成员和成员间的紧密合作比过程和工具更加重要
+ * 优秀的成员是成功的关键
+ *
+ * working software over comprehensive documentation
+ * 可以工作的软件比纤细的文档更加重要
+ *
+ * customer collaboration over contract negotiation
+ * 跟客户合作比合同更加重要
+ *
+ * responding to change over following a plan
+ * 响应变更比遵循计划更加重要
+ *
+ *
+ */
+public class Manifesto {
+}
