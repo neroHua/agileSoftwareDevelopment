40690ed806aec061b49b0bb9bacc19d13f1d5c5c
neroHua <2676337735@qq.com>
2021-12-15 23:07:56 +0800

    add principles

---
+++ src/main/java/chapter01/Principles.java
@@ -0,0 +1,45 @@
+package chapter01;
+
+/**
+ * principles For Agile Software Development
+ * 敏捷开发遵循的原则
+ *
+ * our highest priority is to satisfy the customer through early and continuous delivery of valuable software
+ * 我们最高的优先级原则是：通过更早额且持续的提交有价值的软件来使客户得到满足
+ *
+ * welcome changing requirements, even late in development. agile processes harness change for the customer's competitive advantage.
+ * 欢迎改变需求， 即使是在开发的默契。敏捷开发在改变需求的时候也能为客户取得竞争力的优势
+ *
+ * deliver working software frequently, from a couple of weeks to a couple of months, with a preference to the shorter time scale.
+ * 经常交互可以工作的软件，交付的间隔从几周到几个月，交付的时间间隔越短越好
+ *
+ * business people and developers must work together daily throughout the project.
+ * 在项目周期内，业务人员和开发人员需要每天在一起工作
+ *
+ * build project around motivated individuals. give them the environment and support they need, and trust them to get the job done.
+ * 通过有动力的人员来构建项目。给他们提供环境和相关支持，并且信任他们能够完成工作
+ *
+ * the most efficient and effective method of conveying information to and within a development team is face-to-face conversation.
+ * 最有效的传递信息的方式是通过面对面的交流
+ *
+ * working software is the primary measure of progress
+ * 可以工作的软件是主要衡量进度的进度
+ *
+ * agile processes promote sustainable development. the sponsors, developers and users should be able to maintain a constant pace indefinitely.
+ * 敏捷过程促进可持续的开发。责任人，开发人员，用户保持一个长期的恒定的开发速度
+ *
+ * continuous attention to technical excellence and good design enhances agility.
+ * 持续的关注优秀的技能和好的设计，可以增强敏捷能力
+ *
+ * simplicity the art of maximizing the amount of work not done is essential.
+ * 简单的完成未完成的工作是一种艺术
+ *
+ * the best architectures, requirements, and designs emerge from self-organizing teams.
+ * 最好的架构，需求和设计出自于自组织的团队
+ *
+ * at regular intervals, the team reflects on how to become more effective, then tunes and adjusts its behavior accordingly.
+ * 每隔一段时间，团队需要反思和调整使团队变得更加有效
+ *
+ */
+public class Principles {
+}
