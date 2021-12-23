5c70a31fb81e6242f7812130c67b006251d5ab18
neroHua <2676337735@qq.com>
2021-12-23 21:51:53 +0800

    add extrme programming

---
+++ src/main/java/chapter02/OverviewOfExtremeProgramming.java
@@ -0,0 +1,61 @@
+package chapter02;
+
+/**
+ * overview of extreme programming
+ * 极限编程概述
+ *
+ * customer team member
+ * 客户做为团队成员
+ *
+ * user stories
+ * 用户需求以及用户根据开发者的反馈决定需求的处理的计划
+ *
+ * short cycles
+ * 短周期
+ *      the iteration plan
+ *      每两周交付一次
+ *      the release plan
+ *      每个季度的发布计划
+ *
+ * acceptance tests
+ * 验收测试
+ *
+ * pair programming
+ * 成群结队的编程
+ *
+ * test driven development
+ * 测试驱动编程
+ *
+ * collective ownership
+ * 任何人都拥有代码
+ *
+ * continuous integration
+ * 非阻塞的持续集成代码
+ *
+ * sustainable pace
+ * 可持续的稳定的开发速度
+ *
+ * open workspace
+ * 开发的工作空间
+ *
+ * the planning game
+ * 有计划的进行项目
+ *
+ * simple design
+ * 尽量简单的设计
+ *      consider the simplest thing that could possibly work
+ *      考虑能工作的最简单设计
+ *      you aren't going to need it
+ *      设计简单且满足现在的要求就好了
+ *      once and only once
+ *      不要有重复代码
+ *
+ * refactor
+ * 定时重构
+ *
+ * metaphor
+ * 项目的蓝图
+ *
+ */
+public class OverviewOfExtremeProgramming {
+}
