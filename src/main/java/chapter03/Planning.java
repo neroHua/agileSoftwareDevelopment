7392c688938a0f464813a1bd8dcddf6fc3a5f58e
neroHua <2676337735@qq.com>
2021-12-23 22:08:41 +0800

    add planning

---
+++ src/main/java/chapter03/Planning.java
@@ -0,0 +1,31 @@
+package chapter03;
+
+/**
+ * planning
+ * 项目规划
+ *
+ * initial exploration
+ * 初始探索
+ *      在一个适应过程以后，将任务细分到比较小的功能点，针对这些小功能点做计划
+ *
+ * release plan
+ * 每个季度的发布计划
+ *
+ * iteration plan
+ * 每两周的迭代计划
+ *
+ * task planning
+ * 任务计划
+ *      最小的功能点即是一个任务, 通常开发人员4-16小时以内可以完成
+ *
+ * the halfway point
+ * 中点
+ *      在计划进行到一半的时候，重新评估计划，并作出调整
+ *
+ * iterating
+ * 循环进行
+ *      每次迭代完结，拿到用户的反馈,并作出调整
+ *
+ */
+public class Planning {
+}
