2ab726bcb3b1a956f315b20ace294044acf7f353
neroHua <2676337735@qq.com>
2022-01-05 09:11:45 +0800

    add chapter09

---
+++ src/main/java/section02/chapter09/OpenClosedPrinciple.java
@@ -0,0 +1,18 @@
+package section02.chapter09;
+
+/**
+ * open closed principle
+ * 开闭原则
+ *
+ * software entities(classes, modules, functions) should be open for extension, but closed for modification.
+ * 软件的实体(类， 模块， 函数)应该是可以扩展的，但是是不可以修改的
+ *
+ * 每次迭代时，不需要修改原来的代码，新增代码就好了
+ *
+ * 最重要的是合理的抽象
+ *
+ * 追求100%的开闭原则是困难的
+ *
+ */
+public class OpenClosedPrinciple {
+}
