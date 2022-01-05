5c4e3c967a867fd75c1cc3578b729fbb5902da90
neroHua <2676337735@qq.com>
2022-01-05 22:32:08 +0800

    add section03

---
+++ src/main/java/section03/Section03.java
@@ -0,0 +1,38 @@
+package section03;
+
+/**
+ * employee 雇员表
+ * id 标识
+ * name 名称
+ * id_card 身份证
+ * month_wage 月薪
+ * hour_wage 时薪
+ * sales_wage 计件薪资
+ * pay_way 支付方式
+ * post_address 邮寄地址
+ * bank 银行
+ * bank_count 银行账户
+ * week_pay 每周应支付
+ *
+ * time_card 小时工打卡表
+ * employee_id 雇员标识
+ * start_time 开始时间
+ * finish_time 结束时间
+ * date 打卡日期
+ *
+ * sales_card 销售记录表
+ * employee_id 雇员标识
+ * count 销售数量
+ * date 销售日期
+ *
+ * union_card 工会服务费表
+ * employee_id 雇员标识
+ * money 服务费
+ * date 日期
+ *
+ * 当我首先设计出数据库以后，很快的找到了大师设计的数据库。
+ * 看着差不多，有些沾沾自喜。但是后来发现，大师首先设计的是用例然后是应用程序最后是数据库
+ *
+ */
+public class Section03 {
+}
