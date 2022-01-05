8a829b5aeaf9aabec3fe7155705f15808e96c70d
neroHua <2676337735@qq.com>
2022-01-05 22:02:44 +0800

    add chapter12

---
+++ src/main/java/section02/chapter12/InterfaceSegregationPrinciple.java
@@ -0,0 +1,13 @@
+package section02.chapter12;
+
+/**
+ * interface segregation principle
+ * 接口隔离原则
+ *
+ * clients should not be forced to depend on methods that they do not use
+ * 客户端不应该被强迫依赖于那些不使用的接口
+ *
+ *
+ */
+public class InterfaceSegregationPrinciple {
+}
