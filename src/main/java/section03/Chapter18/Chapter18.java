c2d9eeff4f36f429542fd1b57be172f34592de46
neroHua <2676337735@qq.com>
2022-01-06 00:17:37 +0800

    add chatper18 and rename chapter19

---
+++ src/main/java/section03/Chapter18/Chapter18.java
@@ -0,0 +1,8 @@
+package section03.Chapter18;
+
+/**
+ * 为什么要先设计用例，在设计实现，最后才是关心数据库
+ * 为什么我设计的时候总是会先设计数据库？
+ */
+public class Chapter18 {
+}
--- src/main/java/section03/chapter19/Chapter19.java
+++ src/main/java/section03/chapter19/Chapter19.java
@@ -4,5 +4,5 @@ package section03.chapter19;
  * 程序员不做商务决策
  *
  */
-public class Chapter20 {
+public class Chapter19 {
 }
