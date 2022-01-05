61730559d1f6e357bb65fe2ddc8aa0c585daa20c
neroHua <2676337735@qq.com>
2022-01-05 08:59:58 +0800

    add chapter08

---
+++ src/main/java/section02/chapter08/SingleResponsibilityPrinciple.java
@@ -0,0 +1,14 @@
+package section02.chapter08;
+
+/**
+ * single responsibility principle
+ * 单一职责原则
+ *
+ * a class should have only one reason to change
+ * 一个类仅有一个改变的因素
+ *
+ * 如果一个因素导致两个方向上更改，这两个方向可以放在同一个类里面
+ *
+ */
+public class SingleResponsibilityPrinciple {
+}
