293f562bf17881b0416ba6d19688121abea36010
neroHua <2676337735@qq.com>
2022-01-05 21:47:04 +0800

    add chapter11

---
+++ src/main/java/section02/chapter11/DependencyInversionPrinciple.java
@@ -0,0 +1,16 @@
+package section02.chapter11;
+
+/**
+ * dependency inversion principle
+ * 依赖倒置原则
+ *
+ * a. high-level modules should not depend on low-level modules. both should depend on abstraction.
+ * a. 高层木块不应该依赖于底层模块。二者都应该依赖于抽象。
+ * b. abstractions should not depend on details. details should depend on abstractions.
+ * b. 抽象不应该依赖于细节，细节应该依赖与抽象。
+ *
+ * 简单的说，高层定义接口，底层实现接口
+ *
+ */
+public class DependencyInversionPrinciple {
+}
